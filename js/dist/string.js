c64fe6cabdd2bc70a297c0d2ee72fd69454d739f
aureooms <aurelien.ooms@gmail.com>
2014-10-13 10:13:59 +0200

    build

--- js/dist/string.js
+++ js/dist/string.js
@@ -3,6 +3,26 @@
 	'use strict';
 
 
-/* js/src/dummy.js */
+/* js/src/palyndrom.js */
+
+
+var palyndrom = function ( s, i, j ) {
+
+	var m, n, k;
+
+	n = j - i;
+	m = Math.floor( n / 2 );
+
+	for ( k = 0 ; k < m ; ++k ) {
+		if ( s[i + k] !== s[j - k - 1] ) {
+			return false;
+		}
+	}
+
+	return true;
+
+}
+
+exports.palyndrom = palyndrom;
 
 })(typeof exports === 'undefined' ? this['string'] = {} : exports);
