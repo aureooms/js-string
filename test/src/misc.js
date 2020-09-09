1c294c37330cb50a54a38a147c25b139cac3960e
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-09 16:42:02 +0200

    :art: style: Format misc test.

--- test/src/misc.js
+++ test/src/misc.js
@@ -1,12 +1,12 @@
 import test from 'ava';
 import * as string from '../../src';
-test('string', t => {
-	var c = '7';
-	var n = 10;
-	var mul = string.mul;
+test('fill', t => {
+	const c = '7';
+	const n = 10;
+	const mul = string.mul;
 
-	var lfill = string.__lfill__(c, n, mul);
-	var rfill = string.__rfill__(c, n, mul);
+	const lfill = string.__lfill__(c, n, mul);
+	const rfill = string.__rfill__(c, n, mul);
 
 	t.deepEqual(lfill('jjj'), '7777777jjj', 'lfill');
 	t.deepEqual(rfill('jjj'), 'jjj7777777', 'rfill');
